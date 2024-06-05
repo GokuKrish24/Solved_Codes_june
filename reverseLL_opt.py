b86acaef4d34798a3bdd565c39ed0119aeadf6ac
Gokul Krshna V <gokulvasudevan24@gmail.com>
2024-06-05 11:49:59 +0530

    Create reverseLL_opt.py

---
+++ reverseLL_opt.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def insert(self,data):
+        if(self.head==None):
+            self.head=Node(data)
+        else:
+            temp=self.head
+            while(temp.next!=None):
+                temp=temp.next
+            curr=Node(data)
+            temp.next=curr
+    def Print(self):
+        temp=self.head
+        while(temp!=None):
+            print(temp.data,end="-->")
+            temp=temp.next
+        print()
+    def reverseLL(self):
+        prev=None
+        curr=self.head
+        while(curr!=None):
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
+        self.head=prev
+LL=LinkedList()
+LL.insert(2)
+LL.insert(3)
+LL.insert(4)
+LL.Print()
+LL.reverseLL()
+LL.Print()
