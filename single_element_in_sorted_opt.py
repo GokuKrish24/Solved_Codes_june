f3eb17b931b25afe0080056653e4d3c184e763c0
Gokul Krshna V <gokulvasudevan24@gmail.com>
2024-06-15 22:58:26 +0530

    Create single_element_in_sorted_opt.py

---
+++ single_element_in_sorted_opt.py
@@ -0,0 +1,18 @@
+def singleNonDuplicate(nums):
+        if(len(nums)==1):
+            return nums[0]
+        if(nums[0]!=nums[1]):
+            return nums[0]
+        if(nums[len(nums)-1]!=nums[len(nums)-2]):
+            return nums[len(nums)-1]
+        low=0
+        high=len(nums)-1
+        while(low<=high):
+            mid=(low+high)//2
+            if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]):
+                return nums[mid]
+            elif((mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2==1 and nums[mid]==nums[mid-1])):
+                low=mid+1
+            else:
+                high=mid-1
+        return 0
